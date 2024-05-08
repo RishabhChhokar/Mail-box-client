f3251ee545127cb7214616874f3661ff254bdc49
Rishabh Chhokar <rishabhchokkar@gmail.com>
2024-05-08 10:15:38 +0530

    Added custom hook for inbox component.

--- src/components/Mail/Inbox.jsx
+++ src/components/Mail/Inbox.jsx
@@ -1,59 +1,9 @@
-import { useEffect, useState } from "react";
-import { db } from "../../firebase/firebaseConfig";
-import {
-  collection,
-  query,
-  where,
-  onSnapshot,
-  updateDoc,
-  doc,
-  deleteDoc,
-} from "firebase/firestore";
-import { useSelector, useDispatch } from "react-redux";
+import {useInbox} from "../../customHooks/MailHooks/useInbox"
 import { ListGroup } from "react-bootstrap";
 import { convertFromRaw } from "draft-js";
-import { updateUnreadEmails } from "../../store/auth-slice";
 
 const Inbox = () => {
-  const [emails, setEmails] = useState([]);
-  const userEmail = useSelector((state) => state.auth.userEmail);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const unsubscribe = listenToInboxChanges();
-
-    return () => {
-      unsubscribe();
-    };
-  }, [userEmail]);
-
-  const listenToInboxChanges = () => {
-    const q = query(
-      collection(db, userEmail + "_receivedEmails"),
-      where("to", "==", userEmail)
-    );
-
-    return onSnapshot(q, (snapshot) => {
-      const newEmails = snapshot.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      }));
-      setEmails(newEmails);
-
-      const unreadCount = newEmails.filter((email) => !email.read).length;
-      dispatch(updateUnreadEmails(unreadCount));
-    });
-  };
-
-  const markAsRead = async (id) => {
-    const emailRef = doc(db, userEmail + "_receivedEmails", id);
-    await updateDoc(emailRef, { read: true });
-  };
-
-  const deleteEmail = async (id) => {
-    const emailRef = doc(db, userEmail + "_receivedEmails", id);
-    await deleteDoc(emailRef);
-  };
+  const { emails, markAsRead, deleteEmail } = useInbox();
 
   return (
     <ListGroup>
